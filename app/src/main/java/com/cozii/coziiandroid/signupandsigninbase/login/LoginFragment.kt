48a293eafad4dc37d154ff8ec272e9d97214c8ae
Melvin <melvinjohn1864@gmail.com>
2020-07-08 10:31:02 -0400

    Cozii Android: Change the functionality to accomodate the navigation host fragment and its actions.

--- app/src/main/java/com/cozii/coziiandroid/signupandsigninbase/login/LoginFragment.kt
+++ app/src/main/java/com/cozii/coziiandroid/signupandsigninbase/login/LoginFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.cozii.coziiandroid.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment() {
@@ -17,5 +19,14 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tv_sign_up.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_loginFragment3_to_registerFirstFragment2);
+        }
     }
 }
