1bc5561dafe8d3c92c87a27e210e9e8d361b91d0
Melvin <melvinjohn1864@gmail.com>
2020-07-08 14:47:41 -0400

    Cozii Android: Create custom class for textview, edit text and radio button.

---
+++ app/src/main/java/com/cozii/coziiandroid/coziicustomviews/CoziiTextView.kt
@@ -0,0 +1,27 @@
+package com.cozii.coziiandroid.coziicustomviews
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import com.cozii.coziiandroid.R
+
+class CoziiTextView : AppCompatTextView {
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        setFont(context,attrs)
+    }
+
+    private fun setFont(context: Context, attrs: AttributeSet?) {
+        val array = context.obtainStyledAttributes(attrs, R.styleable.CoziiCustomView)
+        val font = array.getInt(R.styleable.CoziiCustomView_font_name,-1)
+        setCustomFontType(context,font)
+        array.recycle()
+    }
+
+    private fun setCustomFontType(context: Context, font: Int) {
+        val fontType = Font?.setTypeFont(context,font)
+        typeface = fontType
+    }
+}
