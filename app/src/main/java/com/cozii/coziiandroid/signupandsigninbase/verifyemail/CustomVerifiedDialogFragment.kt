0f41e5253b91f53a8515fc9080d84aec0126d616
Melvin <melvinjohn1864@gmail.com>
2020-07-20 12:00:47 -0400

    Cozii Android: Call the activity intent

--- app/src/main/java/com/cozii/coziiandroid/signupandsigninbase/verifyemail/CustomVerifiedDialogFragment.kt
+++ app/src/main/java/com/cozii/coziiandroid/signupandsigninbase/verifyemail/CustomVerifiedDialogFragment.kt
@@ -1,11 +1,13 @@
 package com.cozii.coziiandroid.signupandsigninbase.verifyemail
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.cozii.coziiandroid.R
+import com.cozii.coziiandroid.home.CoziiHomeActivity
 import kotlinx.android.synthetic.main.layout_email_verification_dialog.*
 
 class CustomVerifiedDialogFragment : DialogFragment() {
@@ -23,7 +25,9 @@ class CustomVerifiedDialogFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         verified_next_button.setOnClickListener{
-
+            val intent = Intent(activity,CoziiHomeActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
         }
     }
 
