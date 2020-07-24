34032be9c0ae803fd673c5c210a19f33345b5c22
Melvin <melvinjohn1864@gmail.com>
2020-07-24 13:51:09 -0400

    Cozii Android: Add the fragment and view model functions

--- app/src/main/java/com/cozii/coziiandroid/profile/ProfileFragment.kt
+++ app/src/main/java/com/cozii/coziiandroid/profile/ProfileFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.cozii.coziiandroid.R
 import com.cozii.coziiandroid.home.CoziiHomeActivity
+import com.cozii.coziiandroid.home.viewmodel.HomeSharedViewModel
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
+
+    private val homeViewModel: HomeSharedViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +36,10 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         (activity as CoziiHomeActivity).updateToolbarTitle(getString(R.string.profile_title))
+
+        rv_three_steps_verification.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = VerificationListAdapter(homeViewModel.setVerificationList(context))
+        }
     }
 }
