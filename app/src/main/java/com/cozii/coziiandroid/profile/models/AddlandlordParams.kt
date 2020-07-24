f33d2393871a023d205ef2e40d0b818f0154c24a
Melvin <melvinjohn1864@gmail.com>
2020-07-24 13:47:43 -0400

    Cozii Android: Add the interface and models needed for the profile screen.

---
+++ app/src/main/java/com/cozii/coziiandroid/profile/models/AddlandlordParams.kt
@@ -0,0 +1,3 @@
+package com.cozii.coziiandroid.profile.models
+
+data class AddlandlordParams(val title: String) : ProfileInterface
---
+++ app/src/main/java/com/cozii/coziiandroid/profile/models/ProfileOptions.kt
@@ -0,0 +1,5 @@
+package com.cozii.coziiandroid.profile.models
+
+import android.graphics.drawable.Drawable
+
+data class ProfileOptions(val profileOptionIcon: Drawable?, val profileOption: String) : ProfileInterface
