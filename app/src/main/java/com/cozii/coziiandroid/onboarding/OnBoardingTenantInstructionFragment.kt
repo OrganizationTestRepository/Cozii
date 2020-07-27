0b5712f2a55bc75be6bf9751ecfd93229a9b6425
Hank <hank.j.wu@gmail.com>
2020-07-27 12:05:10 -0400

    added screen

--- app/src/main/java/com/cozii/coziiandroid/onboarding/OnBoardingTenantInstructionFragment.kt
+++ app/src/main/java/com/cozii/coziiandroid/onboarding/OnBoardingTenantInstructionFragment.kt
@@ -1,34 +1,42 @@
 package com.cozii.coziiandroid.onboarding
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 
 import com.cozii.coziiandroid.R
+import kotlinx.android.synthetic.main.fragment_onboarding_0.*
+
 
 class OnBoardingTenantInstructionFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = OnBoardingTenantInstructionFragment()
-    }
 
-    private lateinit var viewModel: OnBoardingTenantInstructionViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.on_boarding_tenant_instruction_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel =
-            ViewModelProviders.of(this).get(OnBoardingTenantInstructionViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        tv_on_board_next.setOnClickListener{
+            it.findNavController().navigate(R.id.action_onBoardFirstFragment_to_onBoardingTenantFragment)
+
+        }
     }
 
 }
