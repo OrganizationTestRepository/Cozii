39ff8139c8243130f9b69e406f6df4d4cdc7e68a
Melvin <melvinjohn1864@gmail.com>
2020-07-27 16:00:56 -0400

    Cozii Android: Delete the unnecessary resources

--- app/src/main/java/com/cozii/coziiandroid/onboarding/OnBoardingTenantInstructionViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.cozii.coziiandroid.onboarding
-
-import androidx.lifecycle.ViewModel
-
-class OnBoardingTenantInstructionViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
