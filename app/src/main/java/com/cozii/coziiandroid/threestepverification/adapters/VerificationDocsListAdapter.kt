034699b722abcc45c17cb0da1485bb02a7d34a74
Melvin <melvinjohn1864@gmail.com>
2020-07-30 10:31:30 -0400

    Cozii Android: Add the adapter for document list.

---
+++ app/src/main/java/com/cozii/coziiandroid/threestepverification/adapters/VerificationDocsListAdapter.kt
@@ -0,0 +1,48 @@
+package com.cozii.coziiandroid.threestepverification.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.cozii.coziiandroid.R
+import com.cozii.coziiandroid.coziicustomviews.CoziiTextView
+import com.cozii.coziiandroid.home.tenant.models.TenantHomeOptions
+import com.cozii.coziiandroid.threestepverification.models.VerificationDocs
+
+class VerificationDocsListAdapter(private val verificationDocs: List<VerificationDocs>) :
+    RecyclerView.Adapter<VerificationDocsListAdapter.VerificationDocViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): VerificationDocsListAdapter.VerificationDocViewHolder {
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_single_document_selection, parent, false)
+        return VerificationDocViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return verificationDocs.size
+    }
+
+    override fun onBindViewHolder(
+        holder: VerificationDocsListAdapter.VerificationDocViewHolder,
+        position: Int
+    ) {
+        holder.bindView(verificationDocs[position])
+    }
+
+    class VerificationDocViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(verificationDoc: VerificationDocs) {
+            val docImage = itemView.findViewById(R.id.iv_document_image) as ImageView
+            val documentName = itemView.findViewById(R.id.tv_document_title) as CoziiTextView
+
+            docImage.setImageDrawable(verificationDoc.verificationDocImage)
+            documentName.text = verificationDoc.verificationDocName
+        }
+
+    }
+}
