3e1ee422e0e2870ad88ef083be495abd33f885ce
Hank <hank.j.wu@gmail.com>
2020-08-05 16:41:35 -0400

    added viewpager 2 changes

--- app/src/main/java/com/cozii/coziiandroid/onboarding/OnBoardingAdapter.kt
+++ app/src/main/java/com/cozii/coziiandroid/onboarding/OnBoardingAdapter.kt
@@ -1,5 +1,10 @@
 package com.cozii.coziiandroid.onboarding
 
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.cozii.coziiandroid.onboarding.OnBoardingTenantFragment.Companion.getInstance
+
 class OnBoardingAdapter(activity: AppCompatActivity, private val itemsCount: Int) :
     FragmentStateAdapter(activity) {
 
@@ -8,6 +13,6 @@ class OnBoardingAdapter(activity: AppCompatActivity, private val itemsCount: Int
     }
 
     override fun createFragment(position: Int): Fragment {
-        return OnBoardingFragment.getInstance(position)
+        return OnboardingMainFragment.getInstance(position)
     }
 }
--- app/src/main/java/com/cozii/coziiandroid/onboarding/OnBoardingLandingActivity.kt
+++ app/src/main/java/com/cozii/coziiandroid/onboarding/OnBoardingLandingActivity.kt
@@ -4,22 +4,58 @@ import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
+import androidx.viewpager2.widget.ViewPager2
 import com.cozii.coziiandroid.R
 
-class OnBoardingLandingActivity : AppCompatActivity() {
+class OnBoardingActivity : AppCompatActivity() {
+
+    private var onBoardingPageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            updateCircleMarker(binding, position)
+        }
+    }
+    private lateinit var binding: ActivityOnBoardingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.onboard_landing_activity)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val numberOfScreens = resources.getStringArray(R.array.on_boarding_titles).size
+        binding.onBoardingMainContainer.makeStatusBarTransparent()
+        val onBoardingAdapter = OnBoardingAdapter(this, numberOfScreens)
+        binding.onBoardingViewPager.adapter = onBoardingAdapter
+        binding.onBoardingViewPager.registerOnPageChangeCallback(onBoardingPageChangeCallback)
     }
 
-    fun updateStatusBarColor(color : String){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
-            val window = window
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.statusBarColor = Color.parseColor(color)
+    private fun updateCircleMarker(binding: ActivityOnBoardingBinding, position: Int) {
+        when (position) {
+            0 -> {
+                binding.onBoardingInitialCircle.background = getDrawable(R.drawable.bg_green_circle)
+                binding.onBoardingMiddleCircle.background = getDrawable(R.drawable.bg_gray_circle)
+                binding.onBoardingLastCircle.background = getDrawable(R.drawable.bg_gray_circle)
+            }
+            1 -> {
+                binding.onBoardingInitialCircle.background = getDrawable(R.drawable.bg_gray_circle)
+                binding.onBoardingMiddleCircle.background = getDrawable(R.drawable.bg_green_circle)
+                binding.onBoardingLastCircle.background = getDrawable(R.drawable.bg_gray_circle)
+            }
+            2 -> {
+                binding.onBoardingInitialCircle.background = getDrawable(R.drawable.bg_gray_circle)
+                binding.onBoardingMiddleCircle.background = getDrawable(R.drawable.bg_gray_circle)
+                binding.onBoardingLastCircle.background = getDrawable(R.drawable.bg_green_circle)
+            }
         }
+    }
+
+    override fun onDestroy() {
+        binding.onBoardingViewPager.unregisterOnPageChangeCallback(onBoardingPageChangeCallback)
+        super.onDestroy()
+    }
+
+    private fun View.makeStatusBarTransparent() {
+        this.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
     }
 }
