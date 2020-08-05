cb81db6f29966680a75791c9c2b188fad14948da
Melvin <melvinjohn1864@gmail.com>
2020-08-05 16:33:42 -0400

    Cozii Android: Create the onboarding adapter class

---
+++ app/src/main/java/com/cozii/coziiandroid/onboarding/adapter/OnBoardingAdapter.kt
@@ -0,0 +1,16 @@
+package com.cozii.coziiandroid.onboarding.adapter
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.cozii.coziiandroid.onboarding.OnBoardingTenantFragment
+
+class OnBoardingAdapter(activity: AppCompatActivity, private val itemsCount: Int) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return itemsCount
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return OnBoardingTenantFragment.getInstance(position)
+    }
+}
