f378f691b25ba3c4c9d52eccfd52c256dc4c5c0c
Melvin <melvinjohn1864@gmail.com>
2020-08-11 11:44:49 -0400

    Cozii Android: Create the functions required in the login fragment

--- app/src/main/java/com/cozii/coziiandroid/signupandsigninbase/viewmodel/SignUpAndSignInSharedViewModel.kt
+++ app/src/main/java/com/cozii/coziiandroid/signupandsigninbase/viewmodel/SignUpAndSignInSharedViewModel.kt
@@ -1,42 +1,23 @@
 package com.cozii.coziiandroid.signupandsigninbase.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import java.util.regex.Pattern
+import com.cozii.coziiandroid.signupandsigninbase.login.models.LoginFields
+import com.cozii.coziiandroid.signupandsigninbase.login.models.LoginForm
 
 class SignUpAndSignInSharedViewModel : ViewModel() {
+    private var loginForm = LoginForm()
 
-    // validating email id
-    private fun isValidEmail(email: String): Boolean {
-        val PATTERN = ("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
-        val pattern = Pattern.compile(PATTERN)
-        val matcher = pattern.matcher(email)
-        return matcher.matches()
+
+    fun getLogin(): LoginForm{
+        return loginForm
     }
 
-
-    fun validateLoginCredentials(username: String,password: String): Boolean {
-        if (!isValidEmail(username) || username == null) {
-            return false
-        }
-
-        if (password == null && password.length < 6) {
-            return false
-        }
-
-        return true
+    fun onLoginButtonClick() {
+        loginForm.onClick()
     }
 
-    fun validateFirstRegisterCredentials(firstname: String,lastname: String): Boolean {
-        if (firstname == null && firstname.length < 2) {
-            return false
-        }
-
-        if (lastname == null && lastname.length < 2) {
-            return false
-        }
-
-        return true
+    fun getLoginFields(): MutableLiveData<LoginFields>{
+        return loginForm.getLoginFields()
     }
-
 }
