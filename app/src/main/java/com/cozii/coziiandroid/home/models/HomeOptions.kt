a74d6d1bc728c8fc9742e85c925965686489d00c
Melvin <melvinjohn1864@gmail.com>
2020-08-13 11:39:42 -0400

    Cozii Android: Create the model class common for landlord and tenant and change the packages

---
+++ app/src/main/java/com/cozii/coziiandroid/home/models/HomeOptions.kt
@@ -0,0 +1,5 @@
+package com.cozii.coziiandroid.home.models
+
+import android.graphics.drawable.Drawable
+
+data class HomeOptions(val optionImage: Drawable?, val option: String, val dividerVisibility : Int)
