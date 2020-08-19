66975d34fefebb431cfb76a7fa2d6403902e12ed
Melvin <melvinjohn1864@gmail.com>
2020-08-19 14:16:38 -0400

    Cozii Android: Convert the fragment layout to data binding layouts

--- app/src/main/java/com/cozii/coziiandroid/signupandsigninbase/register/RegisterSecondFragment.kt
+++ app/src/main/java/com/cozii/coziiandroid/signupandsigninbase/register/RegisterSecondFragment.kt
@@ -5,23 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 
 import com.cozii.coziiandroid.R
+import com.cozii.coziiandroid.databinding.FragmentRegisterSecondBinding
 import com.cozii.coziiandroid.signupandsigninbase.SignUpAndSignInBaseActivity
+import com.cozii.coziiandroid.signupandsigninbase.register.models.RegisterSecondFields
 import kotlinx.android.synthetic.main.fragment_register_second.*
 
 
 class RegisterSecondFragment : Fragment() {
+
+    private val registerViewModel: RegisterViewModel by activityViewModels()
+
+    private lateinit var binding: FragmentRegisterSecondBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_second, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_register_second, container, false)
+
+        binding.registerSecondViewModel = registerViewModel
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -32,10 +47,14 @@ class RegisterSecondFragment : Fragment() {
             it.findNavController().navigate(R.id.action_registerSecondFragment_to_loginFragment);
         }
 
-        second_register_login_button.setOnClickListener {
-            Navigation.findNavController(it)
-                .navigate(R.id.action_registerSecondFragment_to_verifyEmailOtpFragment);
-        }
+        goToOtpVerificationFragment(this)
+    }
+
+    private fun goToOtpVerificationFragment(registerSecondFragment: RegisterSecondFragment) {
+        registerViewModel.getSecondRegisterFields().observe(viewLifecycleOwner, Observer<RegisterSecondFields> {
+            registerSecondFragment.findNavController()
+                .navigate(R.id.action_registerSecondFragment_to_verifyEmailOtpFragment)
+        })
     }
 
 }
