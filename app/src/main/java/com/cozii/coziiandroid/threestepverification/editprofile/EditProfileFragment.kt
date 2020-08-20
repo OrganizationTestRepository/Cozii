51d99602179b0da1a298cb5bda230a7510cd1011
Melvin <melvinjohn1864@gmail.com>
2020-08-20 15:45:37 -0400

    Cozii Android: Convert to data binding layout

--- app/src/main/java/com/cozii/coziiandroid/threestepverification/editprofile/EditProfileFragment.kt
+++ app/src/main/java/com/cozii/coziiandroid/threestepverification/editprofile/EditProfileFragment.kt
@@ -5,19 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 
 import com.cozii.coziiandroid.R
+import com.cozii.coziiandroid.databinding.FragmentEditProfileBinding
 import com.cozii.coziiandroid.home.CoziiHomeActivity
 
 
 class EditProfileFragment : Fragment() {
 
+    private val editProfileViewModel: EditProfileViewModel by activityViewModels()
+
+    private lateinit var binding: FragmentEditProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_edit_profile, container, false)
+
+        binding.editProfileViewModel = editProfileViewModel
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
